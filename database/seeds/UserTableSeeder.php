ea19d9a0ad8e8e7e26b3168759a490d6bef66dd6
DianEkaPrasetya <92441076+DianEkaPrasetya@users.noreply.github.com>
2023-06-20 19:56:03 +0700

    init

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $administrator = new App\User;
+        $administrator->name = 'DIAN';
+        $administrator->email = 'deka2826@gmail.com';
+        $administrator->password = Hash::make('admin');
+        $administrator->nik = '0000000000000000';
+        $administrator->address = 'Desa Sidowayah Rt 07/Rw 03';
+        $administrator->phone ='085156543036';
+        $administrator->roles =json_encode(['ADMIN']);
+        $administrator->status = 'SUDAH';
+
+        $administrator->save();
+
+        $this->command->info('User Admin sudah diinsert');
+    }
+}
